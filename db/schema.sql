b86f013430ed2e5bbcd6cdb2354a0f2c128a8c20
evgeniya <zena28@gmail.com>
2022-10-05 11:02:16 +1030

    add img column to schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,7 @@ CREATE DATABASE keto_recipes;
 CREATE TABLE recipes(
   id SERIAL PRIMARY KEY,
   name TEXT,
+  img TEXT,
   flavour TEXT,
   ingredients TEXT,
   cooking_method TEXT
@@ -11,7 +12,7 @@ CREATE TABLE recipes(
 
 INSERT INTO recipes(name, flavour, ingredients, cooking_method)
 VALUES
-  ('Crack Keto Biscuits', 'sweet', '4 oz cream cheese
+  ('Crack Keto Biscuits', 'https://images.unsplash.com/photo-1590671886400-8f8088b97cb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTV8MjYzOTY0MDh8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60', 'sweet', '4 oz cream cheese
 2 cups shredded mozzarella cheese
 2 tsp baking powder
 3 eggs
--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const PORT = 3000
 app.listen(PORT,
   () => console.log(`server listening to port ${PORT}`)
 )
+// models
+const Recipe = require('./models/recipe')
+
+//routes
+app.get('/api/recipes', (req, res) => {
+  Recipe
+    .findAll()
+    .then(recipes => res.json(recipes))
+})
