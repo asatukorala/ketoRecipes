bbb6b54aa5d8a4e31bd5f93b9cbcdb2d2d080d89
evgeniya <zena28@gmail.com>
2022-10-05 14:54:14 +1030

    server file update

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+// middlewares
+const logger = require('./middlewares/logger')
+
+//controllers
 const recipeController = require('.controllers/recipes_controller')
+
+// models
+const Recipe = require('./models/recipe')
 
 app.listen(PORT,
   () => console.log(`server listening to port ${PORT}`)
 )
-// models
-const Recipe = require('./models/recipe')
 
-const logger = require('./middlewares/logger')
+
+
 
 
 app.use(logger)
-//routes
+
+app.use(express.static('client'))
+
+//middleware
 app.use('/api/recipe', recipeController)
