e11f967ed4ff0721f84a304a4d7fcee11e7af10b
evgeniya <zena28@gmail.com>
2022-10-05 23:01:22 +1030

    server file changed

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const PORT = process.env.PORT || 3000;
 
 // middlewares
 const logger = require('./middlewares/logger')
+const sessions = require('./middlewares/sessions')
 
 //controllers
-const recipeController = require('./controllers/recipes_controller')
+const recipesController = require('./controllers/recipes_controller')
+const usersController = require('./controllers/users_controller')
+const sessionsController = require('./controllers/sessions_controller')
 
 // models
 const Recipe = require('./models/recipe')
@@ -17,10 +20,14 @@ app.listen(PORT,
 
 app.use(logger)
 
+
 app.use(express.static('client'))
 
 app.use(express.json())
 
+app.use(sessions)
+
 //middleware for controllers 
 app.use('/api/recipes', recipeController)
 app.use('/api/users', usersController)
+app.use('/api/sessions', sessionsController)
