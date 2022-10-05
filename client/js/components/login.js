ea99f1a6916458274aa1b1e85cc2454d6d50502a
George <Georgesimbe@icloud.com>
2022-10-05 23:26:41 +1030

    Fixed errors but broke login and sign

--- client/js/components/login.js
+++ client/js/components/login.js
@@ -27,6 +27,7 @@ function login(event) {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   })
+    .then(res => res.json())
     .then(res => {
       if (res.error) {
         renderLogin()
