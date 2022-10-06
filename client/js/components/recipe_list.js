789bbb15b61fc3be2fb80fdc29cf6abdb62eecd0
asela <asela.atukorala@gmail.com>
2022-10-06 12:08:34 +1030

    changing content based on logged state

--- client/js/components/recipe_list.js
+++ client/js/components/recipe_list.js
@@ -7,22 +7,36 @@ function renderRecipeList() {
 }
 
 function renderRecipes() {
-
-  return state.recipes.map(recipe => `
-
-  <section class="recipe" data-id='${recipe.id}'>
-  <header> 
-    <h2>${recipe.name})</h2>
-    <h3>${recipe.flavour}</h3>
-  </header>
-  <span onClick="deleteRecipe(event)">delete</span>
-  <img src="${recipe.img}>"
-  <br>
-  <p> ${recipe.ingredients} </p>
-  <p> ${recipe.cooking_method} </p>
-  </section>
-  `).join('')
-
+  if (state.loggedInUserName != null) {
+    return state.recipes.map(recipe => `
+      ${state.loggedInUserName}
+      <section class="recipe" data-id='${recipe.id}'>
+      <header> 
+        <h2>${recipe.name})</h2>
+        <h3>${recipe.flavour}</h3>
+      </header>
+      <span onClick="deleteRecipe(event)">delete</span>
+      <img src="${recipe.img}>"
+      <br>
+      <p> ${recipe.ingredients} </p>
+      <p> ${recipe.cooking_method} </p>
+      </section>
+      `).join('')
+  } else {
+    return state.recipes.map(recipe => `
+      ${state.loggedInUserName}
+      <section class="recipe" data-id='${recipe.id}'>
+      <header> 
+        <h2>${recipe.name})</h2>
+      </header>
+      <span onClick="deleteRecipe(event)">delete</span>
+      <img src="${recipe.img}>"
+      <br>
+      <p> ${recipe.ingredients} </p>
+      <p> ${recipe.cooking_method} </p>
+      </section>
+      `).join('')
+  }
 }
 
 function deleteRecipe(event) {
